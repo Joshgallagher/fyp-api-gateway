bd9a482ec4bb7b8bc2e48a349535f2dbcecd52fc
Josh <joshgallagher24@gmail.com>
2019-12-24 02:37:44 +0000

    WIP. This commit contains the intial work for setting up the grpc client proxy with the user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,42 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Headers, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { grpcConfig } from '../services/config/grpc.config';
+import { ClientGrpc, Client } from '@nestjs/microservices';
+import { Metadata, MetadataValue } from 'grpc';
 
 @Controller('user')
 export class UserController {
+    @Client(grpcConfig)
+    client: ClientGrpc;
+
+    userService: any;
+
+    onModuleInit() {
+        this.userService = this.client.getService('UserService');
+    }
+
     @Post()
-    create(@Body() createUserDto: CreateUserDto) {
-        return {};
+    async create(@Headers() headers, @Body() createUserDto: CreateUserDto) {
+        const { authorization } = headers;
+        const { name, email, password } = createUserDto;
+
+        const authMetadata = new Metadata();
+        authMetadata.add('authorization', authorization);
+
+        try {
+            const response = await this.userService
+                .registerUser({ name, email, password }, authMetadata)
+                .toPromise();
+
+            return response;
+        } catch ({ code, metadata, details }) {
+            const errorMetadata = (metadata as Metadata);
+            const error = errorMetadata.get('error')[0];
+
+            if (details === 'VALIDATION_ERROR') {
+                throw new UnauthorizedException(error);
+            }
+        }
     }
 
     @Get(':id')

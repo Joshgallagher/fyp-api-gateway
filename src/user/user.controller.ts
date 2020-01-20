97e88d7292575cc62eab0dac2d2906c2c83dd0bc
Josh <joshgallagher24@gmail.com>
2020-01-20 08:09:57 +0000

    Refactor 'create' user logic into a service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,42 +1,14 @@
-import { Controller, Get, Param, Post, Body, Headers, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Headers } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { userServiceConfig } from '../services/config/user-service.config';
-import { ClientGrpc, Client } from '@nestjs/microservices';
-import { Metadata } from 'grpc';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    @Client(userServiceConfig)
-    client: ClientGrpc;
-
-    userService: any;
-
-    onModuleInit() {
-        this.userService = this.client.getService('UserService');
-    }
+    constructor(private readonly userService: UserService) { }
 
     @Post()
-    async create(@Headers() headers, @Body() createUserDto: CreateUserDto) {
-        const { authorization } = headers;
-        const { name, email, password } = createUserDto;
-
-        const authMetadata = new Metadata();
-        authMetadata.add('authorization', authorization);
-
-        try {
-            const response = await this.userService
-                .registerUser({ name, email, password }, authMetadata)
-                .toPromise();
-
-            return response;
-        } catch ({ code, metadata, details }) {
-            const errorMetadata = (metadata as Metadata);
-            const error = errorMetadata.get('error')[0];
-
-            if (details === 'VALIDATION_ERROR') {
-                throw new UnauthorizedException(error);
-            }
-        }
+    async create(@Headers() headers, @Body() user: CreateUserDto) {
+        return await this.userService.create(headers, user);
     }
 
     @Get(':id')
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { userServiceConfig } from '../services/config/user-service.config';
+import { Client, ClientGrpc } from '@nestjs/microservices';
+import { Metadata } from 'grpc';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    @Client(userServiceConfig)
+    client: ClientGrpc;
+
+    userService: any;
+
+    onModuleInit() {
+        this.userService = this.client.getService('UserService');
+    }
+
+    async create({ authorization }, { name, email, password }: CreateUserDto) {
+        try {
+            const response = await this.userService
+                .registerUser(
+                    { name, email, password },
+                    (new Metadata()).add('authorization', authorization)
+                )
+                .toPromise();
+
+            return response;
+        } catch ({ code, metadata, details }) {
+            const errorMetadata = (metadata as Metadata);
+            const error = errorMetadata.get('error')[0];
+
+            if (details === 'VALIDATION_ERROR') {
+                throw new UnprocessableEntityException(error);
+            }
+        }
+    }
+}

d05e8e6352895033b2c6e3c62aa99fe956ca66a5
Josh <joshgallagher24@gmail.com>
2020-01-20 14:26:40 +0000

    Imported ConfigModule, added relevant env files to gitignore and supplied an empty sample env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { UserService } from './user/user.service';
 import { AuthService } from './auth/auth.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, ConfigModule.forRoot()],
   controllers: [UserController, AuthController],
   providers: [UserService, AuthService],
 })

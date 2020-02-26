94ddfc59f27efa82147a498ed880aeda2369bfa1
Josh <joshgallagher24@gmail.com>
2020-02-26 02:56:02 +0000

    Finished implementing article proxy endpoints

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,12 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, ParseUUIDPipe, Param, Body, Post, Headers, Put, Delete } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 
 @Controller('articles')
 export class ArticlesController {
     constructor(private readonly articlesService: ArticlesService) { }
 
+    @Post()
+    create(
+        @Headers('authorization') token: string,
+        @Body() article: Record<string, any>
+    ): Promise<object> {
+        return this.articlesService.create(token, article);
+    }
+
     @Get()
-    findAll() {
+    findAll(): Promise<Array<object>> {
         return this.articlesService.findAll();
+    }
+
+    @Get('user/:userId')
+    findAllByUser(
+        @Param('userId', new ParseUUIDPipe()) userId: string
+    ): Promise<Array<object>> {
+        return this.articlesService.findAllByUser(userId);
+    }
+
+    @Get(':slug')
+    findOne(@Param('slug') slug: string): Promise<object> {
+        return this.articlesService.findOne(slug);
+    }
+
+    @Put(':slug')
+    update(
+        @Headers('authorization') token: string,
+        @Param('slug') slug: string,
+        @Body() article: Record<string, any>
+    ): Promise<object> {
+        return this.articlesService.update(token, slug, article);
+    }
+
+    @Delete(':slug')
+    delete(
+        @Headers('authorization') token: string,
+        @Param('slug') slug: string
+    ): Promise<void> {
+        return this.articlesService.delete(token, slug);
     }
 }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, HttpService, Inject } from '@nestjs/common';
+import { Injectable, HttpService, Inject, OnModuleInit } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
-import { any } from 'bluebird';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class ArticlesService {
+export class ArticlesService implements OnModuleInit {
+    private articleServiceURL: string;
+
     constructor(
         @Inject('ConfigService')
         private readonly configService: ConfigService,
@@ -14,9 +15,34 @@ export class ArticlesService {
         private readonly userService: UserService,
     ) { }
 
-    async findAll() {
+    onModuleInit() {
+        this.articleServiceURL = this.configService.get<string>('ARTICLE_SERVICE_URL');
+    }
+
+    async create(
+        token: string,
+        article: Record<string, any>
+    ): Promise<object> {
         const { data } = await this.httpService
-            .get(`${this.configService.get<string>('ARTICLE_SERVICE_URL')}/articles`)
+            .post(`${this.articleServiceURL}/articles`, article, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    Authorization: token
+                }
+            })
+            .toPromise();
+
+        const { name } = await this.userService.findOne(data.user_id);
+
+        data['author'] = name;
+
+        return data;
+    }
+
+    async findAll(): Promise<Array<object>> {
+        const { data } = await this.httpService
+            .get(`${this.articleServiceURL}/articles`)
             .toPromise();
 
         let articles: Array<Record<any, any>> = [];
@@ -28,5 +54,72 @@ export class ArticlesService {
         }
 
         return articles;
+    }
+
+    async findAllByUser(userId: string): Promise<Array<object>> {
+        const { data } = await this.httpService
+            .get(`${this.articleServiceURL}/articles/user/${userId}`)
+            .toPromise();
+
+        let articles: Array<Record<any, any>> = [];
+
+        for (let article of data) {
+            const { name } = await this.userService.findOne(article.user_id);
+
+            articles.push({ ...article, author: name });
+        }
+
+        return articles;
+    }
+
+    async findOne(slug: string): Promise<object> {
+        const { data } = await this.httpService
+            .get(`${this.articleServiceURL}/articles/${slug}`)
+            .toPromise();
+
+        const { name } = await this.userService.findOne(data.user_id);
+
+        data['author'] = name;
+
+        return data;
+    }
+
+    async update(
+        token: string,
+        slug: string,
+        article: Record<string, any>
+    ): Promise<object> {
+        const { data } = await this.httpService
+            .put(`${this.articleServiceURL}/articles/${slug}`, article, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    Authorization: token
+                }
+            })
+            .toPromise();
+
+        const { name } = await this.userService.findOne(data.user_id);
+
+        data['author'] = name;
+
+        return data;
+    }
+
+    async delete(
+        token: string,
+        slug: string
+    ): Promise<void> {
+        const { data } = await this.httpService
+            .delete(`${this.articleServiceURL}/articles/${slug}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    Authorization: token
+                }
+            })
+            .toPromise();
+
+        return data;
     }
 }

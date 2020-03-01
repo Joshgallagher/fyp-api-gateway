7315c40d5d886ab5d036e949726b018e545e9972
Josh <joshgallagher24@gmail.com>
2020-03-01 06:03:41 +0000

    Removed unneeded provider

--- src/services/providers/articles-service.provider.ts
+++
@@ -1,6 +0,0 @@
-export const ARTICLES_SERVICE_PROVIDER_TOKEN = 'ARTICLES_SERVICE';
-
-// export const articlesServiceProvider = {
-//     provide: ARTICLES_SERVICE_PROVIDER_TOKEN,
-//     useFactory() => { }
-// };

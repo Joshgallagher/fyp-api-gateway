637e95c1b13a7f5cd22d47447bf0830f29690843
Josh <joshgallagher24@gmail.com>
2020-04-29 19:28:16 +0100

    Configured base Ratings module

--- src/ratings/ratings.module.ts
+++ src/ratings/ratings.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { RatingsController } from './ratings.controller';
 import { RatingsService } from './ratings.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        baseURL: configService.get<string>('RATING_SERVICE_URL')
+      }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [RatingsController],
-  providers: [RatingsService]
+  providers: [RatingsService],
+  exports: [HttpModule]
 })
-export class RatingsModule {}
+export class RatingsModule { }
